ab052ff5efed2d6132def69e1bfea231fa0e1b54
Skikri Hassane <hassaneskikri@gmail.com>
2024-08-25 23:38:04 +0100

    convert from the development to production

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from app import watermark
 
 def create_app():    
     app = Flask(__name__)
-    app.config.from_object(DevelopmentConfig)
+    app.config.from_object(ProductionConfig) # convert from the devlop to product
     app.register_blueprint(watermark.bp)
     return app
 
