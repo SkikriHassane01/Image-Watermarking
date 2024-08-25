270a2699720fefe49537baf6b87b67c8bfb8bd26
Skikri Hassane <hassaneskikri@gmail.com>
2024-08-25 23:37:44 +0100

    remove cv2 and numpy

--- app/watermark.py
+++ app/watermark.py
@@ -3,8 +3,6 @@ from flask import Blueprint, render_template, redirect , request, url_for, curre
 # Blueprint is a way to organize a group of related views and other code. Rather than registering views and other code directly with an application, they are registered with a blueprint. Then the blueprint is registered with the application when it is available in the factory function.
 from PIL import Image 
 import os 
-import cv2
-import numpy as np
 from werkzeug.utils import secure_filename 
 from utils.utils import add_logo, add_text
 
